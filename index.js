2073a723c43cde1c22ad8ed5eaf95871a370ed6c
Maxence Bonnici <maxence.bonnici@epitech.eu>
2023-02-24 20:54:53 +0100

    wave header, follow cursor, parralax effect, glitch effect

---
+++ index.js
@@ -0,0 +1,71 @@
+const simple = document.getElementById('simple');
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const blob = document.getElementById('blob');
+const second = document.getElementById('second');
+const text = document.querySelector('.sec_text');
+
+const text_load = () => {
+    setTimeout(() => {
+        text.textContent = "Maxence";
+    }, 0);
+    setTimeout(() => {
+        text.textContent = "a Freelancer";
+    }, 4000);
+    setTimeout(() => {
+        text.textContent = "a Developer";
+    }, 8000);
+    setTimeout(() => {
+        text.textContent = "a Designer";
+    }, 12000);
+}
+
+text_load();
+setInterval(text_load, 16000);
+
+document.body.onpointermove = event => {
+    let { clientX, clientY } = event;
+    blob.animate({
+        left: `${clientX}px`,
+        top: `${clientY}px`
+    }, { duration: 3000, fill: 'forwards' });
+
+};
+
+window.addEventListener("scroll", function () {
+    let value = window.scrollY;
+    second.style.top = -value * 1 + "px";
+});
+
+simple.onmouseover = event => {
+    let iteration = 0;
+    const interval = setInterval(() => {
+        event.target.innerText = event.target.innerText.split("")
+            .map((letter, index) => {
+                if (index < iteration) {
+                    return event.target.dataset.value[index];
+                }
+                return letters[Math.floor(Math.random() * letters.length)]
+            })
+            .join("");
+        if (iteration >= event.target.dataset.value.length) {
+            clearInterval(interval);
+        }
+        iteration += 1 / 3;
+    }, 50);
+
+}
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+        else {
+            entry.target.classList.remove('show');
+        }
+    })
+})
+
+const hidden = document.querySelectorAll('.hidden');
+hidden.forEach((el) => observer.observe(el));
